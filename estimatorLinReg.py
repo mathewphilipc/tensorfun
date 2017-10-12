eefde02362996124867383539b8d6a2b5be8fb2e
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-12 12:15:08 -0400

    some more linreg

--- estimatorLinReg.py
+++ estimatorLinReg.py
@@ -10,3 +10,12 @@ feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
 # neural network classifiers and regressors. The following code provides an estimator
 # that does linear regression.
 estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+
+# TensorFlow provides many helper methods to read and set up data sets. Here was use two
+# data sets: one for training and one for evaluation. We have to tell the function how
+# many batches of data (num_epochs) we want and how big each batch should be.
+
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
