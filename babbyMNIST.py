d7581d2f75106f363afd5ca2ead35b9a2205487f
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-13 12:33:29 -0400

    Some MNIST fiddling

---
+++ babbyMNIST.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
