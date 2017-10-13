e94a9d0e9478446f5b6080508ca6c10c460e9037
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-13 13:48:10 -0400

    more babbyMNIST

--- babbyMNIST.py
+++ babbyMNIST.py
@@ -5,3 +5,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
