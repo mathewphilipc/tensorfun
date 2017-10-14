5ae12dd033c1c0841e84c596e6b928eac6756eef
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-14 18:05:27 -0400

    More training

--- babbyMNIST.py
+++ babbyMNIST.py
@@ -28,4 +28,5 @@ for _ in range(1000):
 	sess.run(train_step, feed_dict = {x: batch_xs, y_: batch_ys})
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accyracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
