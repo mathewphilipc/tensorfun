02b692c98bc26217d787fa223ba0041f6a1f2afd
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-14 18:02:41 -0400

    Some training

--- babbyMNIST.py
+++ babbyMNIST.py
@@ -17,3 +17,15 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run();
+
+for _ in range(1000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	sess.run(train_step, feed_dict = {x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accyracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
