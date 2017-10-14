81a41c36e9629845eb3bc9e4aa884b50c1dac7f1
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-14 18:11:21 -0400

    more deepMNIST

--- deepMNIST.py
+++ deepMNIST.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+sess = tf.InteractiveSession()
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
