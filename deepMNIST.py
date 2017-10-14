105fb1e17df458549a622648a1e49da7b5454bc5
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-14 18:09:04 -0400

    more deepMNIST

--- deepMNIST.py
+++ deepMNIST.py
@@ -2,3 +2,6 @@ import tensorflow as tf
 import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
