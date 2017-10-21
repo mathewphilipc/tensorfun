da624631bf43ea39d28900eaccf11906b42d8ebf
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-21 10:34:58 -0400

    naive FF net

--- simpleDeepMNIST.py
+++ simpleDeepMNIST.py
@@ -17,4 +17,46 @@ for w, l in [(w1, l1), (w2, l2), (w3, l3)]:
 plt.xlabel('x')
 plt.ylabel('h_w(x)')
 plt.legend(loc=2)
-plt.show()
+# plt.show()
+
+# matrix of weights connecting input layer to hidden layer
+# initialized to example starting weights
+w1 = np.array([[0.2, 0.2, 0.2], [0.4, 0.4, 0.4], [0.6, 0.6, 0.6]])
+
+# matrix of weights connecting hidden layer to output layer
+# formalized as 1x3 matrix
+w2 = np.zeros((1,3))
+w2[0,:] = np.array([0.5, 0.5, 0.5]);
+#print('%g', w2[0])
+
+# biases
+b1 = np.array([0.8, 0.8, 0.8])
+b2 = np.array([0.2])
+
+def f(x):
+	return 1 / (1 + np.exp(-x))
+
+def simple_looped_nn_calc(n_layers, x, w, b):
+	for l in range(n_layers - 1):
+		# Set up input array
+		# If it's the first layer, this will just be the x input vector
+		# Otherwise it will be the output of the previous layer
+		if l==0:
+			node_in = x
+		else:
+			node_in = h
+		h = np.zeros((w[l].shape[0],))
+
+		for i in range(w[l].shape[0]):
+			f_sum = 0
+			for j in range(w[1].shape[1]):
+				f_sum += w[l][i][j] * node_in[j]
+			f_sum += b[l][i]
+			h[i] = f(f_sum)
+	return h
+
+w = [w1, w2]
+b = [b1, b2]
+x = [1.5, 2.0, 3]
+
+print simple_looped_nn_calc(3, x, w, b)
