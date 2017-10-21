ff160ac499cdce4702bd6b24ab5264c6a9b0c5e2
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-21 07:29:13 -0400

    output layer

--- deepMNIST.py
+++ deepMNIST.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import os
 import numpy as np
 
-BIG_NUMBER = 200
+BIG_NUMBER = 1000
 
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
@@ -64,7 +64,7 @@ h_pool2 = max_pool_2x2(h_conv2)
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(hpool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
@@ -79,3 +79,16 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	for i in range(BIG_NUMBER):
+		batch = mnist.train.next_batch(50)
+		if i % 10 == 0:
+			train_accuracy = accuracy.eval(feed_dict={
+				x: batch[0], y_: batch[1], keep_prob: 1.0})
+			print('step %d, training accuracy %g' % (i, train_accuracy))
+		train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+	print('test accuracy %g' % accuracy.eval(feed_dict={
+  		x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
