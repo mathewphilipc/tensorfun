20cf2a6e8e01b7b241be12183d837dc00359f50f
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-21 07:20:11 -0400

    more layers

--- deepMNIST.py
+++ deepMNIST.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import os
 import numpy as np
 
+BIG_NUMBER = 200
+
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 from tensorflow.examples.tutorials.mnist import input_data
@@ -58,3 +60,22 @@ b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(hpool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
