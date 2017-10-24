f9289410294c0c3f739b452f087318be71df33a1
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-24 14:00:27 -0400

    more fiddling

--- tfMNIST.py
+++ tfMNIST.py
@@ -1,3 +1,10 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+learning_rate = 0.5
+epochs = 10
+batch_size = 100
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
