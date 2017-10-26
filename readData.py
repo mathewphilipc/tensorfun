80eb99f53ae50ff63b2dd0443e0b55925af3edee
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-26 08:59:19 -0400

    slowly figuring this out

--- readData.py
+++ readData.py
@@ -3,6 +3,9 @@
 from __future__ import print_function
 import os
 import tensorflow as tf
+import numpy as np
+import math as math
+import argparse
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 #filename_queue = tf.train.string_input_producer(["XORdata.csv"])
@@ -20,8 +23,20 @@ def file_len(fname):
 
 filename = "XORdata.csv"
 
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset')
+# args = parser.parse_args()
+
+
 #setup text reader
 file_length = file_len(filename)
 filename_queue = tf.train.string_input_producer([filename])
 reader = tf.TextLineReader(skip_header_lines=1)
 _, csv_row = reader.read(filename_queue)
+
+# setup CSV decoding
+record_defaults = [[0], [0], [0], [0], [0]]
+col1,col2,col3,col4,col5 = tf.decode_csv(csv_row, record_defaults=record_defaults)
+
+# turn features back into a tensor
+# features = tf.pack([col1,col2,col3,col4,col5])
